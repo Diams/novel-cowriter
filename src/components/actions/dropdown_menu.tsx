f1e0191fd67e3b4c82362bb101aef8f9ff7c600c
Masanao Daimon <diams.rider@gmail.com>
2025-12-18 22:00:59 +0900

    Refactor DropdownMenu to accept children for flexible rendering

--- src/components/actions/dropdown_menu.tsx
+++ src/components/actions/dropdown_menu.tsx
@@ -1,21 +1,11 @@
 import * as RadixDropdownMenu from "@radix-ui/react-dropdown-menu";
 
-export default function DropdownMenu() {
-  return (
-    <RadixDropdownMenu.Root>
-      <RadixDropdownMenu.Trigger>Menu</RadixDropdownMenu.Trigger>
-      <RadixDropdownMenu.Portal>
-        <RadixDropdownMenu.Content>
-          <RadixDropdownMenu.Label>
-            DropdownMenu Content
-          </RadixDropdownMenu.Label>
-          <RadixDropdownMenu.Item>Item 1</RadixDropdownMenu.Item>
-          <RadixDropdownMenu.Separator />
-          <RadixDropdownMenu.Item>Item 2</RadixDropdownMenu.Item>
-        </RadixDropdownMenu.Content>
-      </RadixDropdownMenu.Portal>
-    </RadixDropdownMenu.Root>
-  );
+export default function DropdownMenu({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
+  return <RadixDropdownMenu.Root>{children}</RadixDropdownMenu.Root>;
 }
 
 function Trigger() {
