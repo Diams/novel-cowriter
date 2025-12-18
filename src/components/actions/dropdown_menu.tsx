195895714084399e9940776c0b9cc5dcfe2358cc
Masanao Daimon <diams.rider@gmail.com>
2025-12-18 21:47:56 +0900

    Implement DropdownMenu component using Radix UI

--- src/components/actions/dropdown_menu.tsx
+++ src/components/actions/dropdown_menu.tsx
@@ -1,3 +1,19 @@
+import * as RadixDropdownMenu from "@radix-ui/react-dropdown-menu";
+
 export default function DropdownMenu() {
-  return <div>DropdownMenu</div>;
+  return (
+    <RadixDropdownMenu.Root>
+      <RadixDropdownMenu.Trigger>Menu</RadixDropdownMenu.Trigger>
+      <RadixDropdownMenu.Portal>
+        <RadixDropdownMenu.Content>
+          <RadixDropdownMenu.Label>
+            DropdownMenu Content
+          </RadixDropdownMenu.Label>
+          <RadixDropdownMenu.Item>Item 1</RadixDropdownMenu.Item>
+          <RadixDropdownMenu.Separator />
+          <RadixDropdownMenu.Item>Item 2</RadixDropdownMenu.Item>
+        </RadixDropdownMenu.Content>
+      </RadixDropdownMenu.Portal>
+    </RadixDropdownMenu.Root>
+  );
 }
