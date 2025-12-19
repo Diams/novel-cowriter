af8efc536d54fa14d814c5bb91ecaa3bb67cbaad
Masanao Daimon <diams.rider@gmail.com>
2025-12-20 04:49:11 +0900

    Refactor ProjectMenu component for improved readability and structure

--- src/ui/header/project_menu.tsx
+++ src/ui/header/project_menu.tsx
@@ -12,54 +12,56 @@ import { GetAllChatMessages } from "@/utils/data_accessor/chat_message_data_acce
 
 export default function ProjectMenu() {
   return (
-    <DropdownMenu>
-      <DropdownMenu.Trigger>
-        <div className="flex gap-1 items-center">
-          <IconBaselineDensityMedium size={18} />
-          <div>プロジェクト</div>
+    <>
+      <DropdownMenu>
+        <DropdownMenu.Trigger>
+          <div className="flex gap-1 items-center">
+            <IconBaselineDensityMedium size={18} />
+            <div>プロジェクト</div>
+          </div>
+        </DropdownMenu.Trigger>
+        <div className="mr-2">
+          <DropdownMenu.Content>
+            <DropdownMenu.Item
+              onSelect={() => {
+                const project_data: ProjectData = {
+                  structure_version: 1,
+                  canons: GetAllCanons(),
+                  chat_history: GetAllChatMessages(),
+                };
+                const data_str =
+                  "data:text/json;charset=utf-8," +
+                  encodeURIComponent(JSON.stringify(project_data, null, 2));
+                const download_anchor_node = document.createElement("a");
+                download_anchor_node.setAttribute("href", data_str);
+                const jst_date = new Date(
+                  new Date().getTime() + 9 * 60 * 60 * 1000
+                );
+                download_anchor_node.setAttribute(
+                  "download",
+                  `project_data_${jst_date
+                    .toISOString()
+                    .replace(/[:.]/g, "")}.json`
+                );
+                document.body.appendChild(download_anchor_node);
+                download_anchor_node.click();
+                download_anchor_node.remove();
+              }}
+            >
+              <div className="flex gap-1 items-center">
+                <IconFileExport size={16} />
+                <div>エクスポート</div>
+              </div>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              <div className="flex gap-1 items-center">
+                <IconFileImport size={16} />
+                <div>インポート</div>
+              </div>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
         </div>
-      </DropdownMenu.Trigger>
-      <div className="mr-2">
-        <DropdownMenu.Content>
-          <DropdownMenu.Item
-            onSelect={() => {
-              const project_data: ProjectData = {
-                structure_version: 1,
-                canons: GetAllCanons(),
-                chat_history: GetAllChatMessages(),
-              };
-              const data_str =
-                "data:text/json;charset=utf-8," +
-                encodeURIComponent(JSON.stringify(project_data, null, 2));
-              const download_anchor_node = document.createElement("a");
-              download_anchor_node.setAttribute("href", data_str);
-              const jst_date = new Date(
-                new Date().getTime() + 9 * 60 * 60 * 1000
-              );
-              download_anchor_node.setAttribute(
-                "download",
-                `project_data_${jst_date
-                  .toISOString()
-                  .replace(/[:.]/g, "")}.json`
-              );
-              document.body.appendChild(download_anchor_node);
-              download_anchor_node.click();
-              download_anchor_node.remove();
-            }}
-          >
-            <div className="flex gap-1 items-center">
-              <IconFileExport size={16} />
-              <div>エクスポート</div>
-            </div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item>
-            <div className="flex gap-1 items-center">
-              <IconFileImport size={16} />
-              <div>インポート</div>
-            </div>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </div>
-    </DropdownMenu>
+      </DropdownMenu>
+    </>
   );
 }
